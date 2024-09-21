d7b8aac6c4991b529b2372e82e0a5ba77f370c9e
novlan1 <1576271227@qq.com>
2024-09-22 01:03:37 +0800

    chore: lint

--- packages/press-ui/vue.config.js
+++ packages/press-ui/vue.config.js
@@ -1,19 +1,19 @@
 const path = require('path');
 
-const { DispatchScriptPlugin } = require('@plugin-light/webpack-plugin-dispatch-script')
-const { DispatchVuePlugin } = require('@plugin-light/webpack-plugin-dispatch-vue')
-const { ReplaceContentPlugin } = require('@plugin-light/webpack-plugin-replace-content')
+const { DispatchScriptPlugin } = require('@plugin-light/webpack-plugin-dispatch-script');
+const { DispatchVuePlugin } = require('@plugin-light/webpack-plugin-dispatch-vue');
+const { ReplaceContentPlugin } = require('@plugin-light/webpack-plugin-replace-content');
 
-const { RemToRpxPlugin } = require('@plugin-light/webpack-plugin-rem-to-rpx')
-const { FixNpmPackagePlugin } = require('@plugin-light/webpack-plugin-fix-npm-package')
-const { GenVersionMpPlugin, GenVersionWebPlugin } = require('@plugin-light/webpack-plugin-gen-version')
+const { RemToRpxPlugin } = require('@plugin-light/webpack-plugin-rem-to-rpx');
+const { FixNpmPackagePlugin } = require('@plugin-light/webpack-plugin-fix-npm-package');
+const { GenVersionMpPlugin, GenVersionWebPlugin } = require('@plugin-light/webpack-plugin-gen-version');
 
 const { BUILD_NAME_MAP } = require('t-comm/lib/v-console/config');
-const { LOADER: crossGameStyle } = require('@plugin-light/webpack-loader-cross-game-style')
-const { LOADER: injectDynamicStyleMp } = require('@plugin-light/webpack-loader-inject-dynamic-style-mp')
-const { LOADER: injectDynamicStyleWeb } = require('@plugin-light/webpack-loader-inject-dynamic-style-web')
-const { LOADER: vLazy } = require('@plugin-light/webpack-loader-v-lazy')
-const { LOADER: vueDirective } = require('@plugin-light/webpack-loader-vue-directive')
+const { LOADER: crossGameStyle } = require('@plugin-light/webpack-loader-cross-game-style');
+const { LOADER: injectDynamicStyleMp } = require('@plugin-light/webpack-loader-inject-dynamic-style-mp');
+const { LOADER: injectDynamicStyleWeb } = require('@plugin-light/webpack-loader-inject-dynamic-style-web');
+const { LOADER: vLazy } = require('@plugin-light/webpack-loader-v-lazy');
+const { LOADER: vueDirective } = require('@plugin-light/webpack-loader-vue-directive');
 
 
 const LOADER_MAP = {
@@ -22,7 +22,7 @@ const LOADER_MAP = {
   injectDynamicStyleWeb,
   vLazy,
   vueDirective,
-}
+};
 const plugins = [];
 
 if (process.env.UNI_PLATFORM !== 'h5') {
